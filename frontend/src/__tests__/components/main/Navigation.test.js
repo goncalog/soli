4a5f4bd8118ffbb91549c992dde4594f7b9966a6
goncalog <goncalogordo@gmail.com>
2021-01-19 15:08:53 +0000

    update Navigation component's tests to change displayed links

--- frontend/src/__tests__/components/main/Navigation.test.js
+++ frontend/src/__tests__/components/main/Navigation.test.js
@@ -27,16 +27,9 @@ it('renders Home link correctly', () => {
 
 it('renders the Drivers link correctly', () => {
     const { getByText } = render(<Router><Navigation /></Router>);
-    const evsLink = getByText(/Drivers/);
+    const evsLink = getByText(/Projects/);
     expect(evsLink).toBeInTheDocument();
     expect(evsLink.getAttribute('href')).toBe('/evs');
-});
-
-it('renders the Owners link correctly', () => {
-    const { getByText } = render(<Router><Navigation /></Router>);
-    const contactLink = getByText(/Owners/);
-    expect(contactLink).toBeInTheDocument();
-    expect(contactLink.getAttribute('href')).toBe('/owner/signup');
 });
 
 it('renders the Contact link correctly', () => {
