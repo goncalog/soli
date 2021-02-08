bb3484d53a9121e5dfd8f6a83926785ce1a27626
goncalog <goncalogordo@gmail.com>
2021-02-08 14:58:50 +0000

    update ProjectsContainer to pass investmentAmount prop

--- frontend/src/components/support/ProjectsContainer.js
+++ frontend/src/components/support/ProjectsContainer.js
@@ -24,6 +24,7 @@ export default function ProjectsContainer(props) {
                             title={project.title} 
                             size={project.size} 
                             features={features}
+                            investmentAmount={project.investmentAmount}
                             key={index} 
                         />
                     </Link> 
