cfd471bbc491a12dfe5af9dcd33557d9904d210d
goncalog <goncalogordo@gmail.com>
2021-02-10 11:34:43 +0000

    add login and signup links to Footer component

--- frontend/src/components/main/Footer.js
+++ frontend/src/components/main/Footer.js
@@ -3,14 +3,28 @@ import { Link, withRouter } from 'react-router-dom';
 
 import '../../css/Footer.css';
 
-function Footer() {
+function Footer(props) {
     const date = new Date();
     const year = date.getFullYear();
+    const links = (props.loggedIn) 
+            ? [
+                { name: 'Contact', path: '/contact' },
+                { name: 'Logout', path: '/user/logout' },
+            ]
+            : [
+                { name: 'Contact', path: '/contact' },
+                { name: 'Login', path: '/user/login' },
+                { name: 'Signup', path: '/user/signup' },
+            ];
 
     return (
         <div className="footer">
             <footer className="py-2 bg-dark">
-                <Link className="nav-link" to="/contact">Contact</Link>
+                <div className="links-container">
+                    {links.map((item, i) => {
+                        return <Link className="footer-link" key={i} to={item.path}>{item.name}</Link>;
+                    })}
+                </div>             
                 <p className="m-0 text-white">Copyright &copy; { year } Soli</p>
             </footer>
         </div>
