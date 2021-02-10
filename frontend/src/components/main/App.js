ab34c720d73b84e5580bbfc3b095ec5f9175d6c3
goncalog <goncalogordo@gmail.com>
2021-02-10 10:46:58 +0000

    remove Footer from the App component

--- frontend/src/components/main/App.js
+++ frontend/src/components/main/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Footer from './Footer';
 import AppRouter from './AppRouter';
 
 function App() {
   return (
     <div className="App">
       <AppRouter />
-      <Footer />
     </div>
   );
 }
