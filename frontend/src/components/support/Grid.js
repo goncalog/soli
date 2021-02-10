ecf0b239f2cfce0499c17d3fa08718b56468b930
goncalog <goncalogordo@gmail.com>
2021-02-10 14:34:37 +0000

    add key attribute to Grid's children

--- frontend/src/components/support/Grid.js
+++ frontend/src/components/support/Grid.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Grid(props) {
     return (
         <div className="grid">
-            {props.content.map((components) => {
+            {props.content.map((components, i) => {
                 return (
-                    <div className="grid-element">{components}</div>
+                    <div className="grid-element" key={i}>{components}</div>
                 );
             })}
         </div>
