77ca0fb1b6001ad32773b4e0d0fc8344fbb0ad93
goncalog <goncalogordo@gmail.com>
2021-02-10 13:28:03 +0000

    add Image component to Benefits

--- frontend/src/components/support/Benefit.js
+++ frontend/src/components/support/Benefit.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Image from './Image';
 
 export default class Benefit extends React.Component {
     render() {
         return (
             <div className="benefit">
+                <Image imagePath={this.props.imagePath} />
                 <h5 className="benefit-title">{this.props.benefitTitle}</h5>
                 <p className="benefit-text">{this.props.benefitText}</p>
             </div>
