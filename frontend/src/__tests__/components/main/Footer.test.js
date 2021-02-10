26b242c928b0e95a8dcb9ad74eb3eaf0b68f8bf1
goncalog <goncalogordo@gmail.com>
2021-02-10 10:48:19 +0000

    add Contact link to Footer's tests

--- frontend/src/__tests__/components/main/Footer.test.js
+++ frontend/src/__tests__/components/main/Footer.test.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Footer from '../../../components/main/Footer';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 it('renders one child', () => {
-    const wrapper = shallow(<Footer />);
+    const wrapper = shallow(<Router><Footer /></Router>);
     expect(React.Children.count(wrapper.children())).toEqual(1);
 });
 
-it('renders Title paragraph correctly', () => {
+it('renders Contact link correctly', () => {
+    const { getByText } = render(<Router><Footer /></Router>);
+    const link = getByText(/Contact/);
+    expect(link).toBeInTheDocument();
+    expect(link.getAttribute('href')).toBe('/contact');
+});
+
+it('renders paragraph correctly', () => {
     const date = new Date();
     const year = date.getFullYear();
-    const { getByText } = render(<Footer />);
+    const { getByText } = render(<Router><Footer /></Router>);
     const titleParagraph = getByText(year.toString(), { exact: false });
     expect(titleParagraph).toBeInTheDocument();
 });
