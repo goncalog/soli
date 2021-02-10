0af87e0c4291358033ca2e1890e869a34ee82ea0
goncalog <goncalogordo@gmail.com>
2021-02-10 15:44:32 +0000

    add option to display icons in OrderedList

--- frontend/src/components/support/OrderedList.js
+++ frontend/src/components/support/OrderedList.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Image from './Image';
 
 export default function OrderedList(props) {
     return (
         <div className="ordered-list">
             <ol>
                 {props.listItems.map((item, i) => {
+                    const listItem = props.icons
+                        ? [
+                            <Image key={i} imagePath={props.icons[i]} />,
+                            <p key={i}>{item}</p>
+                        ]
+                        :
+                            <li key={i}>{item}</li>;
+
                     return (
-                        <li key={i}>{item}</li>
+                        <div className="ordered-list-item" key={i}>
+                            {listItem}
+                        </div>
                     );
                 })}
             </ol>
