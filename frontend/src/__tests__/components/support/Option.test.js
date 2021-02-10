61dbdbbc26e24ae225f47e5cec98024a4a5bf9b1
goncalog <goncalogordo@gmail.com>
2021-02-10 17:49:45 +0000

    update Option's tests to include _id prop as input's key

--- frontend/src/__tests__/components/support/Option.test.js
+++ frontend/src/__tests__/components/support/Option.test.js
@@ -1,55 +1,54 @@
 import React from 'react';
-import CheckBox from '../../../components/support/CheckBox';
+import Option from '../../../components/support/Option';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-describe('CheckBox', () => {
+describe('Option', () => {
     let props;
-    let shallowCheckBox;
+    let shallowOption;
     const mockFunction = jest.fn();
-    const checkBox = () => {
-        if (!shallowCheckBox) {
-            shallowCheckBox = shallow(<CheckBox {...props}/>);
+    const option = () => {
+        if (!shallowOption) {
+            shallowOption = shallow(<Option {...props}/>);
         }
-        return shallowCheckBox;
+        return shallowOption;
     }
 
-    // This resets the props and shallowCheckBox variables before every test. 
+    // This resets the props and shallowOption variables before every test. 
     // Otherwise, state from one test would leak into another. 
-    // By setting shallowCheckBox to undefined here, when the next test runs, 
-    // if it calls checkBox, a new CheckBox will be created with the current props.
+    // By setting shallowOption to undefined here, when the next test runs, 
+    // if it calls option, a new Option will be created with the current props.
     beforeEach(() => {
         props = {
-            // option: '21123231',
             options: [{ name: "Test 1", _id: '21123231' }, { name: "Test 2", _id: '435254243' }],
             onChange: mockFunction,
         }
-        shallowCheckBox = undefined;
+        shallowOption = undefined;
     });
 
     test('has 2 children', () => {
-        expect(checkBox().children().length).toEqual(2);
+        expect(option().children().length).toEqual(2);
     });
 
     test('has 2 HTML input elements', () => {
-        const shallowWrapper = checkBox().find('input');
+        const shallowWrapper = option().find('input');
         expect(shallowWrapper.length).toEqual(2);
         shallowWrapper.forEach((node, index) => {
-            expect(node.key()).toBe((index).toString());
+            expect(node.key()).toBe((props.options[index]._id));
             expect(node.prop('id')).toBe((index));
             expect(node.prop('value')).toBe(props.options[index]._id);
-            expect(node.prop('type')).toBe('checkbox');
+            expect(node.prop('type')).toBe('radio');
             node.simulate('change', { target: { id: 'test' } });
             expect(mockFunction).toHaveBeenCalled();
         });
     });
 
     test('has 2 HTML label elements', () => {
-        const shallowWrapper = checkBox().find('label');
+        const shallowWrapper = option().find('label');
         expect(shallowWrapper.length).toEqual(2);
         shallowWrapper.forEach((node, index) => {
-            expect(node.prop('for')).toBe((index));
+            expect(node.prop('for')).toBe((props.options[index]._id));
             expect(node.text()).toBe(props.options[index].name);
         });
     });
