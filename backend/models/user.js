eab47cb2d082d8af070c5632f27c11c1368f2c3f
goncalog <goncalogordo@gmail.com>
2021-02-18 15:21:00 +0000

    update User's investment prop to be a Map of Objects

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     name: { type: String, required: true },
     contact: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
-    investments: { type: Map, of: Number, required: true },
+    investments: { type: Map, of: Object, required: true },
     rating: { type: Number, min: 0, max: 5 },
 });
 
