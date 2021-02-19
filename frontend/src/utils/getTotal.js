e26d936a9211c30fb649e27a6fc7697ea5c19b76
goncalog <goncalogordo@gmail.com>
2021-02-19 12:15:39 +0000

    update getTotal util function to use object with real annual values of each Project

--- frontend/src/utils/getTotal.js
+++ frontend/src/utils/getTotal.js
@@ -1,10 +1,14 @@
 export default function getTotal(investments, projects, prop) {
     const total = projects.reduce((sum, project) => {
-        const projectInvestment = Object.values(investments[project._id]).reduce((sum, amount) => {
-            return sum + amount;
-        }, 0);
-        const projectShare = projectInvestment / project.total_cost;
-        return sum + projectShare * project[prop].reduce((sum, amount) => sum + amount, 0);
+        let annualAmounts = {};
+        let subTotal = 0; 
+        for (let i = project.year_start_production; i <= new Date().getFullYear(); i++) {
+            subTotal += (investments[project._id][i]) ? investments[project._id][i] : 0;
+            const userAnnualShare = subTotal / project.total_cost;
+            annualAmounts[i] = (project[prop][i]) ? userAnnualShare * project[prop][i] : 0;
+        }
+
+        return sum + Object.values(annualAmounts).reduce((sum, amount) => sum + amount, 0);
     }, 0);
 
     return (total < 10) ? Math.round(total * 100) / 100 : Math.round(total);
