65d00b56761889e4481fb4561e7c8b9e5359297c
goncalog <goncalogordo@gmail.com>
2021-03-05 12:02:34 +0000

    add totalCapacity to getTotals util function

--- frontend/src/utils/getTotals.js
+++ frontend/src/utils/getTotals.js
@@ -1,5 +1,6 @@
 import formatNumber from './formatNumber';
 import getTotal from './getTotal';
+import formatTotal from './formatTotal';
 
 export default function getTotals(investments, projects) {
     // Assumes all investments are in £
@@ -9,14 +10,27 @@ export default function getTotals(investments, projects) {
         }, 0);
     }, 0);
 
+    const totalCapacity = projects.reduce((sum, project) => {
+        const investment = Object.values(investments[project._id]).reduce((sum, amount) => sum + amount, 0);
+        const projectTotalCost = (project.historical_total_cost[new Date().getFullYear()]) 
+                ? project.historical_total_cost[new Date().getFullYear()] 
+                : project.historical_total_cost[new Date().getFullYear() - 1];
+        const userShare = investment / projectTotalCost;
+
+        return sum + project.size_kw * userShare;
+    }, 0);
+
     const totalProduced = getTotal(investments, projects, 'real_annual_production_kwh');
     const totalCO2Saved = getTotal(investments, projects, 'real_annual_co2_saved_ton');
     const totalReceived = getTotal(investments, projects, 'real_annual_payments');
+    const totalReturn = 0;
 
     return [
         formatNumber(totalInvested), 
+        formatNumber(formatTotal(totalCapacity)), 
         formatNumber(totalProduced),
         formatNumber(totalCO2Saved),
         formatNumber(totalReceived),
+        formatNumber(totalReturn),
     ];
 }
